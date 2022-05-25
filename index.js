878708a22f55fe8a7223708a27081e8b56888351
safentisAuth <safentis@csd.auth.gr>
2022-05-25 14:50:49 +0300

    Add comments

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ $('document').ready(function(){
     $('#chat-text-input').keydown(function(e){
         if(e.keyCode === 13 && !e.shiftKey){
                 sendChat();
+                //Kill the enter key input
                 return false;
          }  
     });    
@@ -11,14 +12,20 @@ $('document').ready(function(){
 });
 
 function sendChat(){
+    //Get the text in the textarea
     var text = $('#chat-text-input').val();
+    //Empty out the textarea
     $('#chat-text-input').val("");
+
+    //If textarea empty, do nothing
     if (text == "") return;
+
     var today = new Date();
 
     //If hours < 10 then add a zero in front. If minutes <10 then add a zero in front
     var time = (today.getHours()<10?`0${today.getHours()}`:today.getHours()) + ":" + (today.getMinutes()<10?`0${today.getMinutes()}` : today.getMinutes());
 
+    //Create and append the chat message
     var user = 'User0';
     $('<div/>', {
         'class':'chat-item',
@@ -27,6 +34,7 @@ function sendChat(){
             '<div class="chat-item-content">' + `${user}: ` + text + '</div>'
     }).appendTo($('#chat-box'));
 
+    //Scroll to the bottom
     var chatBox = document.getElementById('chat-box');
     chatBox.scrollTop = chatBox.scrollHeight;
 }
